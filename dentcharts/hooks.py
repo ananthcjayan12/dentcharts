7f158511d5845b4d936f79a37210ac1501d0f97f
ananthuuu <ananthcjayan12@gmail.com>
2025-06-30 12:29:10 +0530

    check quick entry

--- dentcharts/hooks.py
+++ dentcharts/hooks.py
@@ -27,8 +27,10 @@ app_license = "mit"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/dentcharts/css/dentcharts.css"
-# app_include_js = "/assets/dentcharts/js/dentcharts.js"
+app_include_css = "/assets/dentcharts/css/dentcharts.css"
+app_include_js = [
+    "/assets/frappe/js/frappe/form/quick_entry.js"
+]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/dentcharts/css/dentcharts.css"
