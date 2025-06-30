d104114176f8ef577710e2896a4dd49489c4486d
ananthuuu <ananthcjayan12@gmail.com>
2025-06-30 11:44:51 +0530

    added invoice button

--- dentcharts/public/js/dental_chart_visual.js
+++ dentcharts/public/js/dental_chart_visual.js
@@ -56,6 +56,29 @@ frappe.ui.form.on('Dental Chart', {
 			frm.add_custom_button(__('Add General Procedure'), function() {
 				add_general_procedure(frm);
 			}, __('Add Treatments'));
+			
+			// Add Generate Invoice button if any procedures exist (planned/in-progress/completed)
+			if ((frm.doc.tooth_procedures || []).length) {
+				frm.add_custom_button(__('Generate Invoice'), function() {
+					let items = frm.doc.tooth_procedures
+						.filter(proc => proc.status !== 'Cancelled')
+						.map(proc => ({
+							procedure_code: proc.procedure_code,
+							description: proc.procedure_name,
+							tooth_number: proc.tooth_number,
+							surface: proc.surface,
+							quantity: 1,
+							amount: proc.actual_fee || proc.standard_fee
+						}));
+					frappe.route_options = {
+						patient: frm.doc.patient,
+						practitioner: frm.doc.dentist,
+						dental_clinic: frm.doc.clinic,
+						invoice_items: items
+					};
+					frappe.new_doc('Invoice');
+				}, __('Actions'));
+			}
 		}
 		
 		// Show dentition type info
